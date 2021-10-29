d0bdde60e3c3d51839442819cebfb5faad47b1f4
phatvungoc <phatvungoc@users.noreply.github.com>
2021-10-29 21:50:29 +0700

    upload todolist to github

---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React, { useContext, useState } from "react";
+import { DataContext } from "./DataProvider";
+
+export default function Footer() {
+    const [checkAll, setCheckAll] = useState(false);
+    const [todos, setTodos] = useContext(DataContext);
+
+    const handleCheckAll = () => {
+        const newTodos = [...todos];
+        newTodos.forEach((todo, index) => {
+            todo.complete = !checkAll;
+        });
+        setTodos(newTodos);
+        setCheckAll(!checkAll);
+    };
+
+    const newTodosComplete = () => {
+        return todos.filter((todo) => todo.complete === false);
+    };
+
+    const deleteTodo = () => {
+        setTodos(newTodosComplete());
+        setCheckAll(false);
+    };
+
+    return (
+        <>
+            {todos.length > 0 ? (
+                <div className="row">
+                    <label htmlFor="all">
+                        <input
+                            type="checkbox"
+                            name="all"
+                            id="all"
+                            onChange={handleCheckAll}
+                            checked={checkAll}
+                        />
+                        All
+                    </label>
+                    <p>You have {newTodosComplete().length} to do</p>
+                    <button id="delete" onClick={deleteTodo}>
+                        Delete
+                    </button>
+                </div>
+            ) : (
+                <h2>Congratulations! Nothings To Do</h2>
+            )}
+        </>
+    );
+}
